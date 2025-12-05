ece3514e6ad37c5c251c74cbf849977d0b95681d
hotakamaeda <hotaka.maeda@gmail.com>
2025-12-05 09:45:06 -0600

    event alerts on monday

--- source/popo_bot_event_alerts.py
+++ source/popo_bot_event_alerts.py
@@ -9,7 +9,7 @@ import pytz  # pip install pytz
 # ---- Config ----
 load_dotenv()
 TOKEN = os.getenv("popo_token")
-GENERAL_CHANNEL_ID = int(os.getenv("general_channel"))
+NEWS_CHANNEL_ID = int(os.getenv("general_channel"))
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -31,28 +31,32 @@ async def send_daily_event_reminders():
 
     eastern = pytz.timezone("US/Eastern")
     today_et = datetime.now(eastern).date()
-    tomorrow_et = (datetime.now(eastern) + timedelta(days=1)).date()
+    this_week_et = (datetime.now(eastern) + timedelta(days=7)).date()
+    today_is_monday = datetime.today().weekday() != 0
 
     todays_events = []
-    tomorrows_events = []
+    this_week_events = []
 
     for event in events:
         start_et = event.start_time.astimezone(eastern).date()
         if start_et == today_et:
             todays_events.append(event)
-        elif start_et == tomorrow_et:
-            tomorrows_events.append(event)
+        elif start_et <= this_week_et:
+            this_week_events.append(event)
 
-    if not todays_events and not tomorrows_events:
+    # print(today_is_monday)
+    # print(todays_events)
+    # print(this_week_events)
+    # raise
+
+    if not todays_events and not today_is_monday:
         return  # send nothing
 
-    channel = bot.get_channel(GENERAL_CHANNEL_ID)
-
-    msg = "## :date: **Upcoming Events!**"
-    await asyncio.sleep(.3)
-    await channel.send(msg)
+    channel = bot.get_channel(NEWS_CHANNEL_ID)
 
     if todays_events:
+        await asyncio.sleep(.3)
+        await channel.send("## :date: **Events Today!**")
         for e in todays_events:
             if hasattr(e, "url"):
                 event_link = e.url
@@ -61,17 +65,17 @@ async def send_daily_event_reminders():
             else:
                 continue
 
-    if tomorrows_events:
-        for e in tomorrows_events:
+    # Weekly event list sent only on Mondays
+    if this_week_events & today_is_monday:
+        await asyncio.sleep(.3)
+        await channel.send("## :date: **Events This Week!**")
+        for e in this_week_events:
             if hasattr(e, "url"):
                 event_link = e.url
                 await asyncio.sleep(.3)
                 await channel.send(event_link)
             else:
                 continue
-
-    # print(msg)
-    # await channel.send(msg)
 
 
 @bot.event
